dd3838d9207c1bab2c341c7a1fc7be779bd92ac4
Eduard Oravkin <>
2021-05-08 16:58:27 +0200

    added PostgreSQL environment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT #json w token (obfuscation of data)
@@ -14,7 +15,9 @@ app = Flask(__name__)
 # Note it only creates the tables that it sees. It creates the table stores because
 # it imports the Store Resource which imports the StoreModel which tells it
 # to create the table 'stores'.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # switches off some changes tracker
 app.secret_key = 'jose'
 api = Api(app)
